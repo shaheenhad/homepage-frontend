6ca8ad2d988fa1bdeeac5070c80cc3bacaf963b5
Shaheen Hadadzadeh <shaheenhad@gmail.com>
2022-12-29 18:35:51 -0500

    continue bookmarks

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,10 +5,19 @@ import mePath from "../../images/about-me.jpg";
 function About() {
   return (
     <div className="about">
-      <img src={mePath} className="about__image"></img>
+      <img
+        alt="site author in nature"
+        src={mePath}
+        className="about__image"
+      ></img>
       <div className="about__text">
-        <h2 className="about__title">About the author</h2>
-        <p className="about__description">it me</p>
+        <h3 className="about__title">About the author</h3>
+        <p className="about__description">
+          This block describes the project author. Here you should indicate your
+          name, what you do, and which development technologies you know. You
+          can also talk about your experience with Practicum, what you learned
+          there, and how you can help potential customers.
+        </p>
       </div>
     </div>
   );
--- src/components/Bookmarks/Bookmarks.js
+++ src/components/Bookmarks/Bookmarks.js
@@ -1,5 +1,11 @@
 import React from "react";
 import "./Bookmarks.css";
+import google from "../../images/google.png";
+import amazon from "../../images/amazon.png";
+import insta from "../../images/insta.png";
+import reddit from "../../images/reddit.png";
+import spotify from "../../images/spotify.png";
+import youtube from "../../images/youtube.png";
 
 function Bookmarks() {
   return (
@@ -7,7 +13,34 @@ function Bookmarks() {
       <h2 className="bookmarks__title">Go Somewhere</h2>
       <div className="bookmarks__links">
         <a className="bookmarks__link" href="https://www.google.com/">
-          search
+          <img className="bookmarks__logo" alt="search logo" src={google}></img>
+        </a>
+        <a className="bookmarks__link" href="https://www.spotify.com/">
+          <img
+            className="bookmarks__logo"
+            alt="spotify logo"
+            src={spotify}
+          ></img>
+        </a>
+        <a className="bookmarks__link" href="https://www.reddit.com/">
+          <img className="bookmarks__logo" alt="reddit logo" src={reddit}></img>
+        </a>
+        <a className="bookmarks__link" href="https://www.instagram.com/">
+          <img
+            className="bookmarks__logo"
+            alt="instagram logo"
+            src={insta}
+          ></img>
+        </a>
+        <a className="bookmarks__link" href="https://www.amazon.com/">
+          <img className="bookmarks__logo" alt="amazon logo" src={amazon}></img>
+        </a>
+        <a className="bookmarks__link" href="https://www.youtube.com/">
+          <img
+            className="bookmarks__logo"
+            alt="youtube logo"
+            src={youtube}
+          ></img>
         </a>
       </div>
     </div>
