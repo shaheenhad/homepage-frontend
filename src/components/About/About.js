8bb4f3da0cf84f55496b135fc9d7ccbf80b28483
Shaheen Hadadzadeh <shaheenhad@gmail.com>
2023-01-09 20:10:41 -0500

    gh pages

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,7 +13,7 @@ function About() {
       <div className="about__text">
         <h3 className="about__title">About the author</h3>
         <p className="about__description">
-          Hi! I'm Shaheen Hadadzadeh, a full stack web developer with a
+          Hello! I'm Shaheen Hadadzadeh, a full stack web developer with a
           background in chemical engineering. I work with React, Express, and
           Node, and am fluent in JavaScript, HTML, and CSS.
         </p>
